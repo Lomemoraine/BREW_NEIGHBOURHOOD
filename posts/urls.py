10292b45e9a0370ab1a3cd77fa020a5811903967
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-17 11:04:03 +0300

    initalcommit:setup starter files

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+  path('', views.home, name='home'),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
