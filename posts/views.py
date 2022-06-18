8489cc81291f8238fa1c17d49932651e20bb66cf
DenisMuga <denismugah5@gmail.com>
2022-06-18 08:34:39 +0300

    Importing key classes from modules into views py

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm, UserUpdateForm, ProfileUpdateForm, NewHoodForm, EditHoodForm, NewBizForm, NewPostForm
+from .models import Profile, Neighbourhood, Business, Post
 
 # Create your views here.
 def home(request):
