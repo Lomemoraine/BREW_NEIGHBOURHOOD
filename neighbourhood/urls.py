20cdcfd000c82f2bc14b78f9547ad4327b4dc3c5
DenisMuga <denismugah5@gmail.com>
2022-06-18 08:26:06 +0300

    Configuring project and app url patterns

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('posts.urls'))
+    path('', include('posts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 from . import views
+from posts import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
   path('', views.home, name='home'),
+  path('accounts/register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('update_profile/', user_views.update_profile, name='update_profile'),
+    path('new_hood/', views.new_hood, name='new_hood'),
+    path('hood/', views.hood, name='hood'),
+    path('edithood/', views.edit_hood, name='edithood'),
+    path('businesses/<id>', views.businesses, name='hoodbusiness'),
+    path('post', views.post, name='post'),
+    path('hoodupdates/<id>', views.posthood, name='hoodupdates'),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+# urlpatterns = format_suffix_patterns(urlpatterns)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
