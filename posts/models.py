f23d6ab1e7d1338c83debede4f7c9d8b2cb01720
DenisMuga <denismugah5@gmail.com>
2022-06-18 07:02:00 +0300

    Designing a model for businesses in the neighbourhood

--- posts/models.py
+++ posts/models.py
@@ -46,3 +46,10 @@ class Neighbourhood(models.Model):
 
     def delete_neighborhood(self):
         self.delete()
+        
+class Business(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default = '')
+    email = models.CharField(max_length=100, default = '')
+    neighbourhood = models.ForeignKey("Neighbourhood",on_delete=models.CASCADE, default='', null=True, blank=True)
+    description = models.TextField( default = '')
