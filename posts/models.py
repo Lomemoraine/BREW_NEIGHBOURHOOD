6232ec31d81dd0d94f48307cdf7c42cdb8fb6800
DenisMuga <denismugah5@gmail.com>
2022-06-18 06:53:54 +0300

    Designing a model and instances for the neighbourhood class

--- posts/models.py
+++ posts/models.py
@@ -28,3 +28,12 @@ def create_user_profile(sender, instance, created, **kwargs):
 
     if created:
         Profile.objects.create(user=instance)
+        
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location= models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile",on_delete=models.CASCADE, related_name = 'hood')
+    description = models.TextField( default = '')
+    hood_logo = models.ImageField( upload_to='images/', blank ='true',default='')
+    emergency_contact=models.CharField(max_length=100,null=True, blank=True)
+    occupants_count = models.IntegerField(null  = True ,blank = True)
