a0e015d3a2e0b19bff3daf3f7c214be42a7c3bd9
DenisMuga <denismugah5@gmail.com>
2022-06-18 06:36:49 +0300

    Defining the profile model and its instances

--- posts/models.py
+++ posts/models.py
@@ -4,3 +4,12 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Profile(models.Model):
+    username = models.CharField(max_length=100, blank =True )
+    bio = models.TextField(max_length=300,blank =True)
+    user = models.OneToOneField(User, on_delete = models.CASCADE , related_name='profile')
+    email = models.CharField(max_length=100, default = '')
+    location = models.CharField(max_length=100,blank =True)
+    neighbourhood = models.ForeignKey("Neighbourhood",on_delete=models.CASCADE, default='', null=True, blank=True)
+    profile_pic = models.ImageField( upload_to='profile/', blank ='true',default='default.png')
