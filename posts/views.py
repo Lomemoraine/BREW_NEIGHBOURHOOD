12a92369225451bd356c80e9aaa9d1c930127960
DenisMuga <denismugah5@gmail.com>
2022-06-18 11:24:26 +0300

    Creating a registration function and post request

--- posts/views.py
+++ posts/views.py
@@ -10,3 +10,17 @@ from .models import Profile, Neighbourhood, Business, Post
 def home(request):
     
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+        else:
+            form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
